89f8d57a51f336d8029d9386be53182522a3d90f
zMatepan <oficialzmateoiz@gmail.com>
2026-09-11 15:20:54 -0300

    Delete trabajo/cuatro_espinas directory

--- trabajo/cuatro_espinas/php/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config.php';
-
-$con = new mysqli(BDhost, BDuser, BDpass, BDname);
-
-if($con->connect_error){
-    die("error: " . $con->connect_error);
-}
-?>
--- trabajo/cuatro_espinas/php/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('BDhost', 'localhost');
-define('BDuser', 'root');
-define('BDpass', '');
-define('BDname', 'hospital_de_clinicas');
-?>
