462d62353341ae394cafe639facbb5aeb5b2c645
zMatepan <oficialzmateoiz@gmail.com>
2026-09-18 15:49:19 -0300

    Delete php directory

--- php/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config.php'; // llama a las constante de las credenciales de la bs.
-
-$con = new mysqli(BDhost, BDuser, BDpass, BDname); // hace la conexion a la bs con las constantes
-
-if($con->connect_error){ //si hay error y se detiene la conexion.
-    die("error: " . $con->connect_error);
-}
-?>
--- php/config.php
+++
@@ -1,6 +0,0 @@
-<?php // las credenciales de la BS.
-define('BDhost', 'localhost');
-define('BDuser', 'root');
-define('BDpass', '');
-define('BDname', 'hospital_de_clinicas');
-?>
